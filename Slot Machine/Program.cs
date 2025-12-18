a0e05b84a3bb09fc373fe6f63421db40068c8a17
Seb <sebastiancopara8@gmail.com>
2025-12-18 18:29:23 +0100

    Add introductory text, add money count variable, add a 3x3 grid for the game

---
+++ Slot Machine/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Slot_Machine;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Let’s play the Slot Machine Game!");
+        Console.WriteLine("\nPlease, enter your amount of money to play, and then press ENTER:");
+        int moneyCount = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("\nNow, please choose your configuration:");
+        Console.WriteLine("\nPress 1 if you only want to play with the center line (cost: €X/per spin)");
+        Console.WriteLine("Press 2 if you want to play with all the horizontal lines (cost: €Y/per spin)");
+        Console.WriteLine("Press 3 if you want to play with all 3 horizontal lines + diagonals (cost: €Z/per spin)");
+
+        int[,] slotMachine = new int [3, 3];
+    }
+}
