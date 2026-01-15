c438b3985a28d7e32ba297a3ea95ffc029b46c78
Seb <sebastiancopara8@gmail.com>
2026-01-16 00:07:34 +0100

    - Add while loop that stops when money runs out. - Add 2 for loops, one to populate the grid, and the second to print it. - Work on option 1

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -13,7 +13,51 @@ class Program
         Console.WriteLine("Press 2 if you want to play with all the horizontal lines (cost: €Y/per spin)");
         Console.WriteLine("Press 3 if you want to play with all 3 horizontal lines + diagonals (cost: €Z/per spin)");
 
-        int[,] slotMachine = new int [3, 3];
+        int[,] slotMachineGrid = new int [3, 3];
         
+        char userOption;
+
+        while (moneyCount != 0)
+        {
+            userOption = Console.ReadKey(true).KeyChar;
+	
+            bool win = true;
+            int centerLine = 3/2;
+
+            for (int row = 0; row < 3; row++)
+            {
+                for (int column = 0; column < 3; column++)
+                {
+                    Random rnd = new Random(); 
+                    slotMachineGrid[row, column] = rnd.Next(1, 3);
+                }
+            }
+
+            for (int row = 0; row < 3; row++)
+            {
+                for (int column = 0; column < 3; column++)
+                {
+                    Console.WriteLine(slotMachineGrid[row, column]);
+                }
+            }
+
+            if (userOption == '1')
+            {
+
+                if (slotMachineGrid[centerLine, 0] == slotMachineGrid[centerLine, 1])
+                {
+                    Console.WriteLine("Wow! You’ve just earned €3!");
+                    //win;
+                    moneyCount += 3;
+                } 
+ 
+                else
+                {
+                    Console.WriteLine("Bad luck :(");
+                    moneyCount -= 1;
+                }
+
+            }
+        }
     }
 }
